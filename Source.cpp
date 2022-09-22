90b7ee969402307e1ccb4c27bbfac51f50ea214d
fsffaeswf <114169974+fsffaeswf@users.noreply.github.com>
2022-09-22 18:20:50 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+int main() {
+//1
+//	int a;
+//	cin >> a;
+//	float b = a / 100;
+//	cout << b;
+
+//2
+//	int a;
+//	cin >> a;
+//	float b = a / 1000;
+//	cout << b;
+
+//3
+//int a;
+//cin >> a;
+//	float b = a / 1024;
+//	cout << b;
+
+//4
+	//int a, b;
+	//cin >> a >> b;
+	//int kol = a / b;
+	//cout << kol;
+
+//5
+	//int a, b;
+	//cin >> a >> b;
+	//int kol = a % b;
+	//cout << kol;
+
+//6
+	/*int a;
+	cin >> a;
+	int des = a / 10;
+	int ed = a % 10;
+	cout << des << ' ' << ed;*/
+
+//7
+	/*int a;
+	cin >> a;
+	int des = a / 10;
+	int ed = a % 10;
+	int sum = des+ed;
+	int proiz = des * ed;
+	cout <<sum << ' ' << proiz;*/
+
+//8
+	/*int a;
+	cin >> a;
+	int des = a / 10;
+	int ed = a % 10;
+	int b = ed * 10 + des;
+	cout << b;*/
+
+//9
+	/*int a;
+	cin >> a;
+	int b = a / 100;
+	cout << b;*/
+
+//10
+	/*int a;
+	cin >> a;
+	int ed = a % 10;
+	int sot = (a / 10) / 10;
+	cout << ed << ' ' << sot;*/
+
+//11
+	/*int a;
+	cin >> a;
+	int ed = a % 10;
+	int des = (a / 10) % 10;
+	int sot = (a / 10) / 10;
+	int sum = ed + des + sot;
+	int proiz = ed * des * sot;
+	cout <<sum << ' ' << proiz;*/
+	
+//12
+	/*int a;
+	cin >> a;
+	int ed = a % 10 * 100;
+	int des = (a / 10) % 10 * 10;
+	int sot = (a / 10) / 10;
+	int obr = ed + des + sot;
+	cout <<obr;*/
+
+//13
+	/*int a;
+	cin >> a;
+	int sot = (a / 10) / 10;
+	int des_ed = a % 100;
+	int ish = des_ed * 10 + sot;
+	cout <<ish;*/
+
+//14
+	/*int a;
+	cin >> a;
+	int ed = a % 10*100;
+	int sot_des = a / 10;
+	int ish = sot_des + ed;
+	cout <<ish;*/
+
+//15
+
+	/*int a;
+	cin >> a;
+	int ed = a % 10;
+	int sot_des = a / 10;
+	int des = sot_des % 10 * 100;
+	int sot = sot_des / 10 * 10;
+	int b = ed + des + sot;
+	cout <<b;*/
+
+//16
+
+    /*int a;
+	cin >> a;
+	int sot = a / 100 * 100;
+	int ed_des = a % 100;
+	int ed = ed_des % 10 * 10;
+	int des = ed_des / 10;
+	int b = ed + des + sot;
+	cout <<b;*/
+
+//17
+	/*int a;
+	cin >> a;
+	int c = a / 100;
+	int b = c % 10;
+	cout << b; */
+
+//18
+//int a;
+//cin >> a;
+//int b = a / 1000;
+//cout << b;
+
+//19
+//int a;
+//cin >> a;
+//int b = a / 60;
+//cout << b;
+
+//20
+// int a;
+//cin >> a;
+//int b = a / 3600;
+//cout << b;
+}
